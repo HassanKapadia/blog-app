36bd8afbed6173b2963d2021b52495ca8a7dbc5a
HassanKapadia <hsn.kapadia@gmail.com>
2025-09-06 18:15:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blogging app using spring
+
+A simple blogging app where users can write articles and also like and comment on any article.
+Users can also follow different authors.
+
+## Requirements
+
+1. JDK-21
+2. Gradle 8.14.3
---
+++ src/main/java/com/hancy/app/BloggingApplication.java
@@ -0,0 +1,12 @@
+package com.hancy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplication.class, args);
+	}
+}
